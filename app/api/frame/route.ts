e7fd8f848f593309c987f9fe8e43e3b657640e1d
Ivan Shumeiko <i.shumeiko@amarkets.com>
2024-06-10 18:57:10 +0300

    fix: fix btn

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -12,12 +12,10 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <title>This is frame 5</title>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmXNWyv5kA1w27xU7EQhAcazW8ivFjouEm7B3k4K92AMaZ/5.png" />
+    
     <meta property="fc:frame:button:1" content="Martian Women" />
     <meta property="fc:frame:button:1:action" content="post_redirect" />
-    <meta property="fc:frame:button:2" content="Blog post Tutorial" />
-    <meta property="fc:frame:button:2:action" content="post_redirect" />
-    <meta property="fc:frame:button:3" content="Video Tutorial" />
-    <meta property="fc:frame:button:3:action" content="post_redirect" />
+    
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
   </head></html>`);
   } else {
